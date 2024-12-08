a46a4120fdf34063100d2dedc4ac8ec885030a50
joannwashere <124937246+joannwashere@users.noreply.github.com>
2024-12-07 23:41:34 -0800

    Update newsbot.py

--- newsbot.py
+++ newsbot.py
@@ -5,7 +5,7 @@ from textblob import TextBlob
 
 # Function to fetch news
 def fetch_news():
-    categories = ["MercuryNews", "NYT", "Forbes", "BBC", "NBC", "CNN", "TheGuardian", "WSJ", "TheMotleyFool", "CBSSports", "YahooSports", "YahooFinance", "TMZ"]
+    categories = ["MercuryNews", "NYT", "Forbes", "BBC", "NBC", "CNN", "ABCNews", "TheGuardian", "WSJ", "TheMotleyFool", "ESPN" "CBSSports", "YahooSports", "YahooFinance", "EOnline", "TMZ"]
     news_list = []
 
     for category in categories:
